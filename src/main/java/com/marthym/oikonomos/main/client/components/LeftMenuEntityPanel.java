71ef142466dd9abf677fb29b5b05d826ca03a59c
Marthym <marthym@github.com>
2013-09-04 17:38:30 +0200

    ADD: Icon set on the leftmenu

--- src/main/java/com/marthym/oikonomos/main/client/components/LeftMenuEntityPanel.java
+++ src/main/java/com/marthym/oikonomos/main/client/components/LeftMenuEntityPanel.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ResourcePrototype;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiConstructor;
 import com.google.gwt.uibinder.client.UiField;
@@ -13,9 +15,10 @@ import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DisclosurePanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.InlineHyperlink;
 import com.google.gwt.user.client.ui.Widget;
-
+import com.marthym.oikonomos.main.client.resources.DashboardViewResource;
 import com.marthym.oikonomos.main.client.view.EnumTypeTranslator;
 import com.marthym.oikonomos.shared.model.LeftMenuEntity;
 import com.marthym.oikonomos.shared.view.data.EntityType;
@@ -28,6 +31,7 @@ public class LeftMenuEntityPanel extends Composite {
 	
 	@UiField SpanElement entityCount;
 	@UiField SpanElement entityName;
+	@UiField Image icon;
 	@UiField DisclosurePanel entityDisPanel;
 	@UiField InlineHyperlink addLink;
 	
@@ -39,6 +43,14 @@ public class LeftMenuEntityPanel extends Composite {
 		this.entityName.setInnerText(EnumTypeTranslator.getTranslation((type)));
 		addLink.setTargetHistoryToken(type.name().toLowerCase());
 		entityDisPanel.getHeader().unsinkEvents(Event.ONCLICK);
+		
+		for (ResourcePrototype resource : DashboardViewResource.INSTANCE.getResources()) {
+			if (resource.getName().endsWith(type.name().toLowerCase())) {
+				icon.setResource((ImageResource)resource);
+				break;
+			}
+		}
+		
 	}
 
 	public void setCount(int count) {
--- src/main/java/com/marthym/oikonomos/main/client/resources/DashboardViewResource.java
+++ src/main/java/com/marthym/oikonomos/main/client/resources/DashboardViewResource.java
@@ -1,12 +1,12 @@
 package com.marthym.oikonomos.main.client.resources;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ClientBundleWithLookup;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.resources.client.ImageResource.ImageOptions;
 
-public interface DashboardViewResource extends ClientBundle {
+public interface DashboardViewResource extends ClientBundleWithLookup {
 	public DashboardViewResource INSTANCE = GWT.create(DashboardViewResource.class);
 
 	@Source("img/logout.png")
@@ -20,12 +20,41 @@ public interface DashboardViewResource extends ClientBundle {
 	@ImageOptions(height=32, width=32)
 	ImageResource profile32();
 
+	@Source("img/lm-accounts.png")
+	@ImageOptions(height=24, width=24)
+	ImageResource lmaccount();
+	
+	@Source("img/lm-budgetary.png")
+	@ImageOptions(height=24, width=24)
+	ImageResource lmbudgetary_line();
+	
+	@Source("img/lm-categories.png")
+	@ImageOptions(height=24, width=24)
+	ImageResource lmcategory();
+
+	@Source("img/lm-payees.png")
+	@ImageOptions(height=24, width=24)
+	ImageResource lmpayee();
+
+	@Source("img/lm-reports.png")
+	@ImageOptions(height=24, width=24)
+	ImageResource lmreport();
+
+	@Source("img/lm-schedulers.png")
+	@ImageOptions(height=24, width=24)
+	ImageResource lmscheduler();
+
+	@Source("img/lm-accounts.png")
+	ImageResource mvAccounts();
+
 	public interface DashboardViewCss extends CssResource {
 	      String content();
 	      String logout();
 	      String clear();
-	      @ClassName("profile-main-view") String profileMainView();
 	      @ClassName("profile-nav-top")String profileNavBar();
+	      @ClassName("main-view") String mainView();
+	      @ClassName("profile-main-view") String profileMainView();
+	      @ClassName("accounts-main-view") String accountsMainView();
 	   }
 
 	@Source("DashboardView.css")
