8062e9967a5db82ed520973b26730aad606a5fe3
Marthym <marthym@github.com>
2013-09-13 16:49:20 +0200

    ADD: Browser can now remember user email (not password)

--- src/main/java/com/marthym/oikonomos/login/client/components/ConnectionForm.java
+++ src/main/java/com/marthym/oikonomos/login/client/components/ConnectionForm.java
@@ -4,11 +4,13 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.ui.HTML;
 import com.marthym.oikonomos.client.i18n.OikonomosConstants;
@@ -25,13 +27,19 @@ public class ConnectionForm extends Composite {
 	@UiField Button btnConnect;
 	@UiField HTML errorHandler;
 	@UiField FormPanel formPanel;
+	@UiField VerticalPanel vPanel;
 	
 	public ConnectionForm() {
+		TextBox newEmail = TextBox.wrap(DOM.getElementById("remember_username"));
 		initWidget(uiBinder.createAndBindUi(this));
 		WelcomeFormsRessource.INSTANCE.style().ensureInjected();
 		
 		OikonomosConstants constants = (OikonomosConstants) GWT.create(OikonomosConstants.class);
-
+		int index = vPanel.getWidgetIndex(email);
+		vPanel.remove(index);
+		email = newEmail;
+		vPanel.insert(email, index);
+		
 		email.getElement().setAttribute("placeholder", constants.email());
 		password.getElement().setAttribute("placeholder", constants.password());
 	}
